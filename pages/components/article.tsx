0165369ff184acbab9311b48e78b41b220206c30
鈴木光太郎 <suzukikoutarou@suzukikoutarounoMacBook-Pro.local>
2020-11-08 12:24:26 +0900

    add some css changes

--- pages/components/article.tsx
+++ pages/components/article.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { translate } from "../../lib/api";
 import Layout from "./layout";
+import styles from "./article.module.css";
 
 export default function Acticle({ data, head }) {
   useEffect(() => {});
@@ -13,19 +14,21 @@ export default function Acticle({ data, head }) {
     <Layout siteTitle={head}>
       <div>
         <h1>{head}</h1>
-        <ul>
+        <ul className={styles.ul}>
           {data?.map((article, index) => (
             <div>
               <li>
                 <div
                   id={index}
-                  className="article"
+                  className={styles.title}
                   onMouseOver={() => fetchTitle(index, article.title)}
                 >
                   {article.title}
                 </div>
                 <br></br>
-                <a href={article.url}>{article.url}</a>
+                <div className={styles.url}>
+                  <a href={article.url}>{article.url}</a>
+                </div>
               </li>
               <br></br>
             </div>
