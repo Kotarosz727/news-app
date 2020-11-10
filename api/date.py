2a8b3d40c2f70409ae6861bcf628e13892b81ccf
鈴木光太郎 <suzukikoutarou@suzukikoutarounoMacBook-Pro.local>
2020-11-10 15:31:41 +0900

    test python api

--- api/date.py
+++ api/date.py
@@ -5,7 +5,7 @@ class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header('Content-type', 'text/json')
     self.end_headers()
     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
