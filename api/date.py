8f1435ed93abc9449e15a214c0aeeb77360c33f4
鈴木光太郎 <suzukikoutarou@suzukikoutarounoMacBook-Pro.local>
2020-11-10 15:45:29 +0900

    test python api

--- api/date.py
+++ api/date.py
@@ -4,9 +4,4 @@ from datetime import datetime
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/json')
-    self.end_headers()
-    msg = {"msg":"test"}
-    self.wfile.write(msg)
-    return
+    return print("Hello world!")
