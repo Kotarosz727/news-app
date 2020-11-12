911443e7102762bcbb636bcb0c7689920d263959
鈴木光太郎 <suzukikoutarou@suzukikoutarounoMacBook-Pro.local>
2020-11-12 19:36:17 +0900

    revise next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ module.exports = {
   env: {
     GUARDIAN_URL: process.env.GUARDIAN_URL,
     NYTIMES_URL: process.env.NYTIMES_URL,
+    NIKKEI_BUSINESS_URL: process.env.NIKKEI_BUSINESS_URL,
+    NIKKEI_URL: process.env.NIKKEI_URL
   },
 };
